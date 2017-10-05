6d36d6191df85da737738f86ae6aac8823e08852
Bragdon Bester <bragdon.bester@gmail.com>
2017-10-05 14:05:25 -0700

    EPPlus Library Testing

--- WaterAnalysisTool/Program.cs
+++ WaterAnalysisTool/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OfficeOpenXml;
 
 namespace WaterAnalysisTool
 {
@@ -10,9 +12,17 @@ namespace WaterAnalysisTool
     {
         static void Main(string[] args)
         {
-            //This is a change
-            Console.WriteLine("CHANGINNNNN'");
-            //Courtney changed something now
+            FileInfo newFile = new FileInfo(@"sample1.xlsx");
+            if(newFile.Exists)
+            {
+                newFile.Delete();
+                newFile = new FileInfo(@"sample1.xlsx");
+            }
+
+            using (ExcelPackage package = new ExcelPackage(newFile))
+            {
+                
+            }
         }
     }
 }
