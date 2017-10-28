c0e4e601427d6e5f2ff06b5b518950abde5b21fc
Daniel Bragdon Bester <bragdon.bester@gmail.com>
2017-10-28 16:44:14 -0700

    Moved code that writes samples to the excel file into a private function (WriteSamples) to get rid of copy paste code. Added functionality to handle all sample types. See TODO's for next steps.

--- WaterAnalysisTool/Loader/DataLoader.cs
+++ WaterAnalysisTool/Loader/DataLoader.cs
@@ -13,7 +13,7 @@ namespace WaterAnalysisTool.Loader
         private List<Sample> CalibrationsStandards;     // Calibration Standard -> Sample Type: Cal
         private List<Sample> QualityControlSamples;     // Stated Values (CCV) -> Sample Type: QC
         private List<Sample> CertifiedValueSamples;     // Certified Values (SoilB/TMDW/etc.) -> Sample Type: QC
-        private List<Sample> Samples;
+        private List<Sample> Samples;                   // Generic Samples -> Sample Type: Unk
 
         private StreamReader Input;
         private ExcelPackage Output;
@@ -25,6 +25,7 @@ namespace WaterAnalysisTool.Loader
             this.Output = outf;
 
             this.CalibrationSamples = new List<Sample>();
+            this.CalibrationsStandards = new List<Sample>();
             this.QualityControlSamples = new List<Sample>();
             this.CertifiedValueSamples = new List<Sample>();
             this.Samples = new List<Sample>();
@@ -86,87 +87,192 @@ namespace WaterAnalysisTool.Loader
 
             // Write QC/QA samples
             int row = 7; // Start at row 7, col 1
-            int count = 0;
-            int rowStart, rowEnd;
+
             if(CalibrationSamples.Count > 0)
-            {
-                dataws.Cells[row, 1].Value = "Quality Control Solutions";
-                dataws.Cells[row, 1].Style.Font.Bold = true;
-                row++;
-                rowStart = row;
+                row = WriteSamples(dataws, CalibrationSamples, nameof(CalibrationSamples), row);
 
-                foreach(Sample s in CalibrationSamples)
-                {
-                    col = 1;
-                    count = 0;
+            if(QualityControlSamples.Count > 0)
+                row = WriteSamples(dataws, QualityControlSamples, nameof(QualityControlSamples), row);
 
-                    dataws.Cells[row, col].Value = s.Name;
-                    dataws.Cells[row, ++col].Value = s.RunTime.Split(' ')[1];
+            if(CertifiedValueSamples.Count > 0)
+                row = WriteSamples(dataws, CertifiedValueSamples, nameof(CertifiedValueSamples), row);
 
-                    foreach(Element e in s.Elements)
-                    {
-                        count++;
-
-                        // Write Analyte concentrations
-                        dataws.Cells[row, col + 1].Value = e.Average;
-                        // TODO apply QA/QC formatting
-
-                        // Write RSD
-                        dataws.Cells[row, col + 1 + s.Elements.Count + 2].Value = e.RSD;
-                        // TODO apply QA/AC formatting
-
-                        col++;
-                    }
-
-                    row++;
-                }
-
-                rowEnd = row - 1;
-
-                dataws.Cells[row, 1].Value = "average";
-                dataws.Cells[row, 1].Style.Font.Bold = true;
-
-                for (col = 3; col <= count + 2; col++)
-                    dataws.Cells[row, col].Formula = "AVERAGE(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
-
-                row++;
-                dataws.Cells[row, 1].Value = "LOD";
-                dataws.Cells[row, 1].Style.Font.Bold = true;
-
-                for(col = 3; col <= count + 2; col++)
-                    dataws.Cells[row, col].Formula = "3*STDEV(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
-
-                row++;
-                dataws.Cells[row, 1].Value = "LOQ";
-                dataws.Cells[row, 1].Style.Font.Bold = true;
-
-                for (col = 3; col <= count + 2; col++)
-                    dataws.Cells[row, col].Formula = "10*STDEV(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
-
-                row+=2;
-            }
+            if(Samples.Count > 0)
+                row = WriteSamples(dataws, Samples, nameof(Samples), row);
         } // end Load
 
-        // TODO
-        // Error checking for Add<Sample> functions
+        #region Add<Sample>
         public void AddCalibrationSample(Sample sample)
         {
+            if (sample == null)
+                throw new ArgumentNullException("Sample is null.\n");
+
             this.CalibrationSamples.Add(sample);
         }
 
         public void AddQualityControlSample(Sample sample)
         {
+            if (sample == null)
+                throw new ArgumentNullException("Sample is null.\n");
+
             this.QualityControlSamples.Add(sample);
         }
 
         public void AddCertifiedValueSample(Sample sample)
         {
+            if (sample == null)
+                throw new ArgumentNullException("Sample is null.\n");
+
             this.CertifiedValueSamples.Add(sample);
         }
 
         public void AddSample(Sample sample)
         {
+            if (sample == null)
+                throw new ArgumentNullException("Sample is null.\n");
+
             this.Samples.Add(sample);
         }
-    }
-}
+        #endregion
+
+        /* Private Methods */
+        private int WriteSamples(ExcelWorksheet dataws, List<Sample> samples, String type, int row)
+        {
+            int count = 0;
+            int rowStart, rowEnd, col;
+
+            switch (type)
+            {
+                case "CalibrationSamples":
+                    dataws.Cells[row, 1].Value = "Quality Control Solutions";
+                    break;
+
+                case "QualityControlSamples":
+                    dataws.Cells[row, 1].Value = "Stated Values";
+                    // TODO figure out what the heck the numbers in this row come from and write them
+                    break;
+
+                case "CertifiedValuesSamples":
+                    dataws.Cells[row, 1].Value = "Certified Values";
+                    // TODO figure out what the heck the numbers in this row come from and write them
+                    break;
+
+                case "Samples":
+                    dataws.Cells[row, 1].Value = "Samples";
+                    break;
+
+                default:
+                    break;
+            }
+
+            dataws.Cells[row, 1].Style.Font.Bold = true;
+
+            row++;
+            rowStart = row;
+
+            foreach (Sample s in samples)
+            {
+                col = 1;
+                count = 0;
+
+                dataws.Cells[row, col].Value = s.Name;
+                dataws.Cells[row, ++col].Value = s.RunTime.Split(' ')[1];
+
+                foreach (Element e in s.Elements)
+                {
+                    count++;
+
+                    // Write Analyte concentrations
+                    dataws.Cells[row, col + 1].Value = e.Average;
+                    // TODO apply QA/QC formatting if applicable to sample type
+
+                    // Write RSD
+                    dataws.Cells[row, col + 1 + s.Elements.Count + 2].Value = e.RSD;
+                    // TODO apply QA/AC formatting if applicable to sample type
+
+                    col++;
+                }
+
+                row++;
+            }
+
+            rowEnd = row - 1;
+
+            #region Write Unique Rows
+            switch (type)
+            {
+                case "CalibrationSamples":
+                    row++;
+                    dataws.Cells[row, 1].Value = "average";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count + 2; col++)
+                        dataws.Cells[row, col].Formula = "AVERAGE(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
+
+                    row++;
+                    dataws.Cells[row, 1].Value = "LOD";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count + 2; col++)
+                        dataws.Cells[row, col].Formula = "3*STDEV(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
+
+                    row++;
+                    dataws.Cells[row, 1].Value = "LOQ";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count + 2; col++)
+                        dataws.Cells[row, col].Formula = "10*STDEV(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
+
+                    break;
+
+                case "QualityControlSamples":
+                    row++;
+                    dataws.Cells[row, 1].Value = "average";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count + 2; col++)
+                        dataws.Cells[row, col].Formula = "AVERAGE(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
+
+                    row++;
+                    dataws.Cells[row, 1].Value = "% difference";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count + 2; col++)
+                        dataws.Cells[row, col].Formula = "(" + dataws.Cells[rowEnd + 1, col].Address + "-" + dataws.Cells[rowStart - 1, col].Address + ")/" + dataws.Cells[rowStart - 1, col].Address + "*100"; // TODO There are extra numbers in the same row as the title "Stated Values"... They are used in this calc
+
+                    break;
+
+                case "CertifiedValueSamples": // TODO figure out how the heck to split up certified values
+                    row++;
+                    dataws.Cells[row, 1].Value = "average";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count + 2; col++)
+                        dataws.Cells[row, col].Formula = "AVERAGE(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")";
+
+                    row++;
+                    dataws.Cells[row, 1].Value = "rsd (%)";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count + 2; col++)
+                        dataws.Cells[row, col].Formula = "STDEV(" + dataws.Cells[rowStart, col].Address + ":" + dataws.Cells[rowEnd, col].Address + ")/" + dataws.Cells[rowEnd + 1, col].Address + "*100";
+
+                    row++;
+                    dataws.Cells[row, 1].Value = "recovery (%)";
+                    dataws.Cells[row, 1].Style.Font.Bold = true;
+
+                    for (col = 3; col <= count; col++)
+                        dataws.Cells[row, col].Formula = dataws.Cells[rowEnd + 1, col].Address + "/" + dataws.Cells[rowStart - 1, col].Address + "*100"; // TODO There are extra numbers in the same row as the title "Certified Values"... They are used in this calc
+
+                    break;
+
+                default:
+                    break;
+
+            }
+            #endregion
+
+            return  row + 2;
+        }// end WriteSamples
+    }// end DataLoader class    
+}// end WaterAnalysisTool.Loader namespace
