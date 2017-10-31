e7acaf9b4249613942e022d49b6d0a757097076f
Wes Brown <mantooth27@gmail.com>
2017-10-30 22:34:12 -0700

    Put TODO back in Parse()

--- WaterAnalysisTool/Loader/DataLoaderParser.cs
+++ WaterAnalysisTool/Loader/DataLoaderParser.cs
@@ -24,6 +24,14 @@ namespace WaterAnalysisTool.Loader
         /* Public Methods */
         public void Parse()
         {
+
+            // TODO
+            // Parse performs the following functions
+            // 1. Read each sample from the input stream
+            //  1.1 Create Sample
+            //  1.2 Add elements to the sample
+            //  1.3 Add the sample to the correct list (using Loader.Add<SampleType> see comments in DataLoader by each list)
+
             string line = "";
             this.Input.ReadLine(); // Consumes the first line of the file that is always empty
 
