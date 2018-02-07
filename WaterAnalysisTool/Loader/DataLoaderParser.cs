24412ec7fe159632cceb595013dffb9c7b028ae0
Brown, Wesley <mantooth27@gmail.com>
2018-02-06 17:55:03 -0800

    ...

--- WaterAnalysisTool/Loader/DataLoaderParser.cs
+++ WaterAnalysisTool/Loader/DataLoaderParser.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Collections.Generic;
 using WaterAnalysisTool.Components;
+using WaterAnalysisTool.Exceptions;
+using OfficeOpenXml;
 
 namespace WaterAnalysisTool.Loader
 {
@@ -14,6 +16,8 @@ namespace WaterAnalysisTool.Loader
 
         private DataLoader Loader;
         private StreamReader Input;
+
+        private List<string> CheckStandardsSampleNames;
 
         private List<Sample> CalibrationStandardsList; // Calibration Standard -> Sample Type: Cal, --- These go in the Calibration Standards worksheet.  Calib Blank, CalibStd
         private List<Sample> CalibrationSamplesList;  // Quality Control Solutions -> Sample Type: QC --- These are usually named Instrument Blank
@@ -31,6 +35,8 @@ namespace WaterAnalysisTool.Loader
             this.Loader = loader;
             this.Input = inf;
 
+            this.CheckStandardsSampleNames = new List<string>();
+
             this.CalibrationStandardsList = new List<Sample>(); // CalibStd
             this.CalibrationSamplesList = new List<Sample>(); // Instrument Blank
             this.QualityControlSamplesList = new List<Sample>(); // CCV
@@ -46,6 +52,7 @@ namespace WaterAnalysisTool.Loader
         public void Parse ()
         {
             this.Input.ReadLine(); // Consumes empty line at the beginning of the file
+            this.ParseCheckStandards();
 
             while (this.Input.Peek() >= 0)
             {
@@ -55,7 +62,6 @@ namespace WaterAnalysisTool.Loader
                 this.AddSampleToList(samp);
             }
 
-            this.CombSampleList();
             this.PassToDataLoader();
         }
 
@@ -105,6 +111,91 @@ namespace WaterAnalysisTool.Loader
 
 
         // Parse() helper methods
+
+
+        private void ParseCheckStandards()
+        {
+            FileInfo fi = new FileInfo("CheckStandards.xlsx");
+            if (!fi.Exists)
+                throw new FileNotFoundException("The CheckStandards.xlsx config file does not exist or could not be found and a calibration curve could not be generated.");
+
+            using (ExcelPackage ep = new ExcelPackage(fi))
+            {
+                ExcelWorksheet ews = ep.Workbook.Worksheets[2];
+                int row = 3; // We start from row 3. Assuming the format of CheckStandards.xlsx will remain the same
+                int col = 3;
+                int analyteCounter = 0;
+                String sampleName;
+                double avg;
+                List<string> elementNames = new List<string>();
+                Element tmpElem;
+                Sample tmpSample;
+
+                while (ews.Cells[row, col].Value != null)
+                {
+                    elementNames.Add(ews.Cells[row, col].Value.ToString()); // Make a list of element names for creation of List<Element> to add to the sample
+                    analyteCounter++;
+                    col++;
+                }
+
+                row = 4;
+                col = 1;
+
+                while (ews.Cells[row, col].Value != null && !(ews.Cells[row, col].Value.ToString().ToLower().Equals("continuing calibration verification (ccv)")))
+                    row++;
+
+                if (row > 14)
+                    throw new ConfigurationErrorException("Could not find Continuing Calibration Verification (CCV) section in CheckStandards.xlsx config file.");
+                else // CCV parsing
+                {
+                    while (ews.Cells[row, col].Value != null)
+                    {
+                        sampleName = ews.Cells[row, col].Value.ToString(); // Reads in the name of the sample
+                        analyteCounter += 2; // Increment by two. Otherwise we could potentially miss the last two columns of analytes due to the value of the column (col) starting at 3 in the for loop below
+                        List<Element> ccvTmpList = new List<Element>();
+
+                        for (col = 3; col < analyteCounter; col++)
+                        {
+                            if (!(double.TryParse(ews.Cells[row, col].Value.ToString(), out avg)))
+                                avg = Double.NaN;
+
+                            tmpElem = new Element(elementNames[col - 3], "", avg, Double.NaN, Double.NaN); // Stddev and RSD get Double.NaN, assuming all values in CheckStandards are avg. Subtract columns (col) by 3 so that the correct corresponding element name is retrieved
+                            ccvTmpList.Add(tmpElem);
+                        }
+
+                        tmpSample = new Sample("", sampleName, "", "", "QC", 0);
+                        this.QualityControlSamplesList.Add(tmpSample);
+                        row++;
+                        col = 1;
+                    }
+                }
+
+                // need condition here to check if file is formatted correctly
+
+                while (ews.Cells[row, col].Value != null) // check standards parsing
+                {
+                    sampleName = ews.Cells[row, col].Value.ToString();
+                    this.CheckStandardsSampleNames.Add(sampleName);
+                    List<Element> checkStandardsTmpList = new List<Element>();
+
+                    for (col = 3; col < analyteCounter; col++)
+                    {
+                        if (!(double.TryParse(ews.Cells[row, col].Value.ToString(), out avg)))
+                            avg = Double.NaN;
+
+                        tmpElem = new Element(elementNames[col - 3], "", avg, Double.NaN, Double.NaN);
+                        checkStandardsTmpList.Add(tmpElem);
+                    }
+
+                    tmpSample = new Sample("", sampleName, "", "", "QC", 0);
+                    List<Sample> certifiedValueTmpList = new List<Sample>();
+                    certifiedValueTmpList.Add(tmpSample);
+                    this.CertifiedValueList.Add(certifiedValueTmpList);
+                    row++;
+                    col = 1;
+                }
+            }
+        }
 
 
         private Sample ParseHeader ()
@@ -236,12 +327,12 @@ namespace WaterAnalysisTool.Loader
             else if (string.Compare(samp.SampleType, "QC") == 0)
             {
                 if (this.CertifiedValueList.Count == 0)
-                    this.CreateNewCertifiedValueSubList(samp);
+                    this.CreateNewCertifiedValueSubList(samp); // Probably won't need this anymore
                 else
                 {
                     foreach (List<Sample> sampleList in this.CertifiedValueList)
                     {
-                        if (samp.Name.StartsWith(sampleList[0].Name.Substring(0, 4))) //
+                        if (samp.Name.StartsWith(sampleList[0].Name.Substring(0, 4))) // SoilB in the input txt file is SoilB. In the CheckStandards.xlsx it is Soil B. Makes it problematic for this line
                             sampleList.Add(samp);
                         else
                             this.CreateNewCertifiedValueSubList(samp);
@@ -277,19 +368,6 @@ namespace WaterAnalysisTool.Loader
             this.Loader.AddCalibrationStandard(this.CreateSampleGroup(this.CalibrationStandardsList, "Calibration Standards", false)); // CalibStd
             this.Loader.AddCalibrationSampleGroup(this.CreateSampleGroup(this.CalibrationSamplesList, "Quality Control Solutions", false)); // Instrument Blank
             this.Loader.AddQualityControlSampleGroup(this.CreateSampleGroup(this.QualityControlSamplesList, "Stated Value", true)); // CCV
-        }
-
-
-        private void CombSampleList () // This method will comb through List<List<Sample>> SampleList and pull out certified value samples that have sample type "Unk", placing them in List<List<Sample>> CertifiedValueList
-        {
-            /*foreach (List<Sample> sampleList in this.SampleList)
-            {
-                foreach (List<Sample> certifiedValueList in this.CertifiedValueList)
-                {
-                    if (sampleList[0].Name.StartsWith(certifiedValueList[0].Name.Substring(0, 4)))
-                        // 
-                }
-            }*/
         }
 
 
